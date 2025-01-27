3dab95fa1821a2eba1a88432f5157390a7145871
jmesplana <johnm.esplana@gmail.com>
2025-01-28 00:15:37 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import BrowserAgentChat from '../components/BrowserAgentChat';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <BrowserAgentChat />
+    </div>
+  );
+}
